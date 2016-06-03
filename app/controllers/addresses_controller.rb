5484caca3bc3904aa28ed5eaded70a15cd6c6373
jbates103 <jimmycbates103@gmail.com>
2016-06-03 10:30:37 -0400

    Add files via upload

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,19 @@
 class AddressesController < ApplicationController
   helper AddressesHelper
-  before_action :set_user, only:[:index, :new, :edit, :create,:update]
-  before_action :set_address, only:[:update]
+  before_action :set_user, only:[:index, :new, :edit, :create, :update]
+  before_action :set_address, only:[:edit, :update]
+
   def index
     @addresses = @user.addresses.all
   end
 
-  def create
-    @address = @user.addresses.new(address_params)
-    if @address.current == 1
-      Address.update_all( current: 0)
+  def create 
+    address = Address.new(address_params)
+    if address.current == 1
+      intTypeAddress = address.type_address == 'work' ? 1 : 0
+      Address.where('type_address = ? AND id <> ?', intTypeAddress, params[:id]).update_all( current: 0)
     end
+    @address = @user.addresses.new(address_params)
     if @address.save!
       flash[:success] = "Address saved successfully!"
       redirect_to user_addresses_path(@user)
@@ -24,19 +27,20 @@ class AddressesController < ApplicationController
     @address = Address.new
   end
 
-  def edit
-    @address = @user.addresses.find(params[:id]) 
+  def edit 
   end
 
   def destroy
   end
 
   def update
-    if @address.current == 1
-      Address.update_all( current: 0)
+    address = Address.new(address_params)
+    if address.current == 1
+      intTypeAddress = address.type_address == 'work' ? 1 : 0
+      Address.where('type_address = ? AND id <> ?', intTypeAddress, params[:id]).update_all(current: 0)
     end
-    @address.update(address_params)
-    if @address.save!
+    @address.update_attributes(address_params)
+    if @address.save
       flash[:success] = "Address updated successfully!"
       redirect_to user_addresses_path(@user)
     else
